56d483e85d64e502c51165cfc3d04a1f5f82e2a8
Dmitry Demenchuk <dmitry@demenchuk.me>
2016-06-07 12:04:50 +0100

    Add progress bar

--- sitemapChecker.js
+++ sitemapChecker.js
@@ -1,26 +1,26 @@
 var fs = require('fs');
 var xml2js = require('xml2js');
 var webshot = require('webshot');
+var progressBar = require('progress');
 
 var base_url = 'http://example.com';
 
-var _save = function(paths) {
+var _save = function(paths, progress) {
   if (paths.length > 0) {
     var path = paths.shift();
     var url = base_url + path + '?_escaped_fragment_=';
     
-    console.log('Requesting', path);
+    progress.tick({path: path});
     
     var options = {
       renderDelay: 1000,
       windowSize: { width: 1200},
-      shotSize: { width: 'window', height: 'window' }  
+      shotSize: { width: 'all', height: 'all' }  
     }
-    
     
     webshot(url, 'screenshots' + path + '.png', options, function(err) {
       if (err) console.error('(!) Cannot load url', path);
-      _save(paths);
+      _save(paths, progress);
     });
   }
 }
@@ -28,8 +28,12 @@ var _save = function(paths) {
 fs.readFile(__dirname + '/sitemap.xml', function(err, data) {
   var parser = new xml2js.Parser();
   parser.parseString(data, function (err, result) {
-    _save(result.urlset.url.map(function(item) {
+    var paths = result.urlset.url.map(function(item) {
       return item.loc[0].replace(base_url, '');
-    }));
+    });
+    
+    var progress = new progressBar('[:bar] :percent :current: :path ', {total: paths.length, width: 20});
+    
+    _save(paths, progress);
   });
 });
